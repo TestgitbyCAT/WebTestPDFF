7772d3ef256fa8b634e4d0064538258de32c5a40
TestgitbyCAT <testcat2019@gmail.com>
2019-02-22 08:59:36 +0700

    openPDF

---
+++ WebTest10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTest10.Startup))]
+namespace WebTest10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
